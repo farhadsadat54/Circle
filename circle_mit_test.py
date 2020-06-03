db598fd35e5083cd40646790eb4b98673cee9e5e
Farhad <farhadsadat54@gmail.com>
2020-06-03 20:33:08 +0200

    mein circle test

---
+++ circle_mit_test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -------------------
+#
+# -------------------
+import unittest
+from math import pi
+
+def circle_area(r):
+    if r < 0:
+        raise ValueError("Der Radius ist leider negativ")
+    if type(r) not in [int, float]:
+        raise TypeError("Nur Zahleneigabe erlaubt")
+
+    flaeche = pi * (r ** 2)
+    return flaeche
+
+
+class TestCircleArea(unittest.TestCase):
+    def test_area(self):
+        self.assertAlmostEqual(circle_area(0), 0)
+
+    def test_values(self):
+        self.assertRaises(ValueError, circle_area, -9)
+
+    def test_types(self):
+        self.assertRaises(TypeError, circle_area, True)
+
+
+r = float(input("Eingabe Radius= "))
+#a = circle_area(r)
+#print(a)
+print("Flächeninhalt= ", circle_area(r))
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
---
+++ circle_ohne_test.py
@@ -0,0 +1,11 @@
+from math import pi
+def circle_area(r):
+    Flaeche = r * r * pi
+    return Flaeche
+r = float(input("Radius= " ))
+print("square= ", circle_area(r))
+
+
+
+
+
